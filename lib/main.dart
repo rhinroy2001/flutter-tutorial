0dff528e0cdad765e1fddb14b5929faa65a71f0c
Ryan Hagen <71740723+rhinroy2001@users.noreply.github.com>
2022-10-20 09:40:23 -0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import './product_manager.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('EasyList'),
+          ),
+          body: ProductManager(),
+        ));
+  }
+}
---
+++ lib/product_manager.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+import './products.dart';
+
+class ProductManager extends StatefulWidget {
+  final String startingProduct;
+
+  ProductManager({this.startingProduct = 'Sweets Tester'});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _ProductManagerState();
+  }
+}
+
+class _ProductManagerState extends State<ProductManager> {
+  List<String> _products = [];
+
+  @override
+  void initState() {
+    _products.add(widget.startingProduct);
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(covariant ProductManager oldWidget) {
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          margin: EdgeInsets.all(10.0),
+          child: ElevatedButton(
+            //color: Theme.of(context).primaryColor,
+            onPressed: () {
+              setState(() {
+                _products.add('Advanced Food Tester');
+              });
+            },
+            child: Text('Add Product'),
+          ),
+        ),
+        Products(_products)
+      ],
+    );
+  }
+}
